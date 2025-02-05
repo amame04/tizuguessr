d258b1453e8da73e2422e7167961cac853296bf3
amame04 <ameame2t@gmail.com>
2025-02-06 08:06:08 +0900

    hyouji

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,82 @@
 import { useEffect, useState } from 'react';
 import './App.css'
-import { getRandomCrossing } from './Overpass';
-import { Map } from "./components/Map";
+import { getPref, getRandomCrossing, reverseGeo, toFormatedName } from './osm';
+import { Map } from './components/map';
+import { Answer } from './components/answer';
 
 function App() {
-  const [data, setData] = useState([null, null])
+  const placeObj = {
+    lat: 0,
+    lng: 0,
+    name: '',
+    formatedName: '',
+    pref: '',
+    isCorrect: false,
+    isAnswered: false
+  }
+
+  const [place, setPlace] = useState(placeObj)
 
   useEffect(() => {
-    getRandomCrossing().then(data => {
-      const lat = data.elements[0].lat
-      const lon = data.elements[0].lon
-      setData([lat, lon]);
-    });
+    let ignore = false;
+
+      getRandomCrossing().then(latlng => {
+        const lat = parseFloat(latlng.elements[0].lat)
+        const lon = parseFloat(latlng.elements[0].lon)
+        reverseGeo(lat, lon).then(name => {
+          if (!ignore) {
+            setPlace({
+              lat: lat,
+              lng: lon,
+              name: name.display_name,
+              formatedName: toFormatedName(name.display_name),
+              pref: getPref(name.display_name),
+              isCorrect: false,
+              isAnswered: false
+            });
+          }
+        })
+      });
+
+    return () => {
+      ignore = true;
+    };
   },[]);
 
-  if (data[0] == null) {
+  function answerButton() {
+    setPlace({
+      ...place,
+      isAnswered: true,
+      isCorrect: true
+    });
+  }
+
+  if (place.lat == 0 && place.lng == 0 && place.name == '') {
     return (
       <div className="App">
         loading...
       </div>
     );
+  } else if (place.isAnswered) {
+    return (
+      <div className="App">
+        <h1 id='logo'>地図げっさー</h1>
+        <div>{place.isCorrect ? '正解！' : '不正解！'}</div>
+        <div>{place.formatedName}</div>
+        <Map lat={place.lat} lng={place.lng} isAnswered={place.isAnswered}/>
+        <Answer disabled={true} />
+        <button onClick={answerButton} disabled>回答</button>
+      </div>
+    );
   } else {
     return (
       <div className="App">
-        <Map lat={data[0]} lng={data[1]}/>
+        <h1 id='logo'>地図げっさー</h1>
+        <Map lat={place.lat} lng={place.lng} isAnswered={place.isAnswered}/>
+        <Answer disabled={false} />
+        <button onClick={answerButton}>回答</button>
       </div>
-    );
+    )
   }
 }
 
---
+++ src/components/map.tsx
@@ -0,0 +1,75 @@
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import Leaflet, { LatLng } from 'leaflet';
+import './map.css';
+import 'leaflet/dist/leaflet.css';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+export const Map = ({ lat, lng, isAnswered }: any) => {
+  const position = new LatLng(lat, lng);
+  const zoom = 16;
+
+  const markerIcon = Leaflet.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+  });
+
+  if (isAnswered) {
+    return (
+      <MapContainer
+        center={position}
+        zoom={zoom}
+      >
+        <Marker position={position} icon={markerIcon} />
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    );
+  } else {
+    return (
+      <MapContainer
+        minZoom={zoom}
+        maxZoom={zoom}
+        center={position}
+        zoom={zoom}
+        dragging={false}
+        scrollWheelZoom={false}
+        boxZoom={false}
+        touchZoom={false}
+        zoomControl={false}
+        doubleClickZoom={false}
+      >
+        <Marker position={position} icon={markerIcon} />
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    );
+  } 
+};
+
+export const AnswerMap = ({ lat, lng }: any) => {
+  const position = new LatLng(lat, lng);
+  const zoom = 16;
+
+  const markerIcon = Leaflet.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+  });
+
+  return (
+    <MapContainer
+      center={position}
+      zoom={zoom}
+    >
+      <Marker position={position} icon={markerIcon} />
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  );
+};

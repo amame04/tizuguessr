ba9f548cb5638fe4bbe34e03285135715bc600bb
amame04 <ameame2t@gmail.com>
2025-02-06 15:21:51 +0900

    design

--- src/components/answer.tsx
+++ src/components/answer.tsx
@@ -55,5 +55,17 @@ export function Answer({ answerButton, disabled } : any) {
         <option value="沖縄県">沖縄県</option>
       </select>
       <button onClick={() => {answerButton(selected)}} disabled={disabled}>回答</button>
+      <ReloadButton />
     </div>
+}
+
+function ReloadButton() {
+  return (
+    <button className="reloadButton" onClick={() => {location.reload()}}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"/>
+        <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466"/>
+      </svg>
+    </button>
+  );
 }
